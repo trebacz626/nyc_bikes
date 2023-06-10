3d1a4953412c29bad79e9c618e9ffa5d20d0a75b
PawelGlr <45963167+PawelGlr@users.noreply.github.com>
2023-06-10 15:01:57 +0200

    Getting weather history

--- scripts/weather.py
+++ scripts/weather.py
@@ -2,8 +2,9 @@ import argparse
 import os
 import requests
 import pandas as pd
+import csv
 def get_url(start_date, end_date):
-    return f"https://archive-api.open-meteo.com/v1/archive?latitude=40.71&longitude=-74.01&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation,rain,snowfall,weathercode,cloudcover,windspeed_10m,winddirection_10m,windgusts_10m"
+    return f"https://archive-api.open-meteo.com/v1/archive?latitude=40.71&longitude=-74.01&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation,rain,snowfall,weathercode,cloudcover,windspeed_10m,winddirection_10m,windgusts_10m&format&format=csv"
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -13,15 +14,15 @@ def main():
     args= parser.parse_args()
     dest = args.dest
     url = get_url(args.from_date, args.to_date)
-    #get json
+
     response = requests.get(url)
-    #parse json to dict
-    data = response.json()['hourly']
-    df = pd.DataFrame(data)
-    #save to csv
-    df.to_csv(dest, index=False)
-
-
+    data = response.content.decode()
+    with open(dest, "w") as file:
+        lines = data.split("\n")
+        for line in lines[3:-1]:
+            file.write(line)
+            file.write("\n")
+        file.write(lines[-1])
 
 
 
